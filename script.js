ae2acf1c76a648ac26ae51803b4f4933658af6c8
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-27 09:33:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	var res = "";
+	var char_list = "ABCDEFGHIJKLMNOPQRSTVUWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(let i=0;i<l;i++){
+		res += char_list.charAt(Math.floor(Math.random()*char_list.length));
+	}
+	return res;
 }
 
 // Do not change the code below.
